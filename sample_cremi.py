d1dedc1918d4b8c53daba31188ed807f0077d4ff
Benedict Au <benau@ymail.com>
2020-04-26 12:36:18 -0500

    first commit

---
+++ sample_cremi.py
@@ -0,0 +1,24 @@
+def sample_cremi(file, z, x, y):
+    """Samples n x n x n cube from cremi data.
+    
+    Args:
+    file: str, cremi file
+    z: int, in [1:125]
+    x: int, in [1:1250]
+    y: int, in [1:1250]
+    """
+
+    with h5py.File(file, 'a') as h5file:
+        print(f"Found file: {file}")
+        image = h5file["/volumes/raw"]
+        print("Found raw image volume")
+        seg = h5file["/volumes/labels/neuron_ids"]
+        print("Found neuron_ids labels volume")
+        image_sample = image[:z, :x, :y]
+        seg_sample = seg[:n, :n, :n]
+        print(f"Created samples: {z}x{x}x{y}")
+        print(f"Creating datasets in {file}")
+        h5file.create_dataset("volumes/sample/raw",  data=image_sample)
+        print("volumes/sample/raw")
+        h5file.create_dataset("volumes/sample/neuron_ids",  data=seg_sample)
+        print("volumes/sample/neuron_ids")
