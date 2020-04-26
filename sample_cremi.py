6ee0d4fd447deef2589c2c415b24a763bf1aff9a
Benedict Au <benau@ymail.com>
2020-04-26 12:42:41 -0500

    fixed bug

--- sample_cremi.py
+++ sample_cremi.py
@@ -1,11 +1,20 @@
+import h5py
+import os
+import numpy as np
+
 def sample_cremi(file, z, x, y):
     """Samples n x n x n cube from cremi data.
+    
+    Appends sampled cube to h5file.f5/volumes/sample/
     
     Args:
     file: str, cremi file
     z: int, in [1:125]
     x: int, in [1:1250]
     y: int, in [1:1250]
+    
+    Example call:
+    sample_cremi("./data_cremi/sample_C_20160501.hdf", 100, 100, 100)
     """
 
     with h5py.File(file, 'a') as h5file:
@@ -15,7 +24,7 @@ def sample_cremi(file, z, x, y):
         seg = h5file["/volumes/labels/neuron_ids"]
         print("Found neuron_ids labels volume")
         image_sample = image[:z, :x, :y]
-        seg_sample = seg[:n, :n, :n]
+        seg_sample = seg[:z, :x, :y]
         print(f"Created samples: {z}x{x}x{y}")
         print(f"Creating datasets in {file}")
         h5file.create_dataset("volumes/sample/raw",  data=image_sample)
